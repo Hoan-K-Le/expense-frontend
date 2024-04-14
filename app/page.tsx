d533b10a40a92e602550e5cd341d442e66bc9825
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2024-04-13 22:49:04 -0700

    Finished home page layout without functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,60 @@
-import Link from "next/link";
+"use client";
+import { signOut } from "next-auth/react";
+import {
+  Moon,
+  LogOutIcon,
+  AddIcon,
+  ChangeLogIcon,
+  AnalyticIcon,
+} from "@/components/svg";
+
+const styles = {
+  container: "bg-white w-1/2 relative",
+  nav: "flex justify-between p-8 items-center",
+  expenseDescriptionContainer:
+    "flex flex-col items-center justify-center min-h-screen",
+  expenseDescriptionWrapper: "h-[52vh] flex flex-col items-center",
+  expenseTitle: "text-gray-500 text-md  mb-8",
+  expensePrice: "text-6xl",
+  expenseFooterContainer: "fixed bottom-0 border-t px-4 py-6 w-1/2",
+  expenseFooterWrapper: "flex justify-around w-full ",
+  btnStyle: "flex flex-col items-center",
+  addIcon: "bg-yellow-200 rounded-full p-2",
+  navTitle: "font-bold",
+};
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <button>{Moon()}</button>
+        <p className={styles.navTitle}>EXPENSES</p>
+        <button onClick={() => signOut()}>{LogOutIcon()}</button>
+      </nav>
+      <div className={styles.expenseDescriptionContainer}>
+        <div className={styles.expenseDescriptionWrapper}>
+          <p className={styles.expenseTitle}>Spent this month</p>
+          <div className="text-red-600  flex gap-1">
+            <span className="text-4xl">$ -</span>{" "}
+            <span className="text-6xl">20</span>{" "}
+            <span className="text-4xl">.00</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.expenseFooterContainer}>
+        <div className={styles.expenseFooterWrapper}>
+          <button className={styles.btnStyle}>
+            <p>{AnalyticIcon()}</p>
+            Analytic
+          </button>
+
+          <button className={styles.addIcon}>{AddIcon()}</button>
+          <button className={styles.btnStyle}>
+            <p>{ChangeLogIcon()}</p>
+            Change logs
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- components/svg.tsx
+++ components/svg.tsx
@@ -25,3 +25,114 @@ export const GoogleLogo = () => {
     </svg>
   );
 };
+
+export const Moon = () => {
+  return (
+    <svg
+      enable-background="new 0 0 512 512"
+      viewBox="0 0 512 512"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-8 w-8 hover:scale-110 transition-all"
+    >
+      <path
+        d="M349.852,343.15c-49.875,49.916-131.083,49.916-181,0c-49.916-49.918-49.916-131.125,0-181.021  c13.209-13.187,29.312-23.25,47.832-29.812c5.834-2.042,12.293-0.562,16.625,3.792c4.376,4.375,5.855,10.833,3.793,16.625  c-12.542,35.375-4,73.666,22.25,99.917c26.209,26.228,64.5,34.75,99.916,22.25c5.792-2.062,12.271-0.582,16.625,3.793  c4.376,4.332,5.834,10.812,3.771,16.625C373.143,313.838,363.06,329.941,349.852,343.15z M191.477,184.754  c-37.438,37.438-37.438,98.354,0,135.771c40,40.021,108.125,36.416,143-8.168c-35.959,2.25-71.375-10.729-97.75-37.084  c-26.375-26.354-39.333-61.771-37.084-97.729C196.769,179.796,194.039,182.192,191.477,184.754z"
+        fill="#1D1D1B"
+      />
+    </svg>
+  );
+};
+
+export const LogOutIcon = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="1.5"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      className="h-5 w-5 hover:scale-110 transition-all"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+      ></path>
+    </svg>
+  );
+};
+
+export const AddIcon = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="1.5"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      className="h-8 w-8 hover:scale-125 transition-all "
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      ></path>
+    </svg>
+  );
+};
+
+export const ChangeLogIcon = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="1.5"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      className="h-6 w-6 hover:scale-125 transition-all"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46"
+      ></path>
+    </svg>
+  );
+};
+
+export const AnalyticIcon = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      stroke-width="1.5"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      className="h-6 w-6 hover:scale-125 transition-all "
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
+      ></path>
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
+      ></path>
+    </svg>
+  );
+};
